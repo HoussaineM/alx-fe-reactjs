88cd3b77000d54b41c79e372f892859483480bc0
HoussaineM <houssinemalme@hotmail.com>
2024-08-11 12:34:34 +0200

    alx react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,16 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function WelcomeMessage() {
-  return (
-      <div>
-          <h1>Welcome to ALX React APP!</h1>
-          <p>This is a simple JSX component.</p>
-      </div>
-  );
-}
 
-export default WelcomeMessage;
 
 function App() {
   const [count, setCount] = useState(0)
