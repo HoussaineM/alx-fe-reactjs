0a5e6874a71bef92daa5cf53f3f2ea19234cdc19
HoussaineM <houssinemalme@hotmail.com>
2024-08-17 19:24:27 +0200

    react

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header(){
     return (
-        <header>
+        <header style={{ backgroundColor: 'blue', padding:"10px", color: 'rgba(200,200,0,1)', textAlign: 'center'}}>
             <h1>My Favorite Cities</h1>
         </header>
     );
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
- const UserProfile = (props) => {
-    console.log(props);
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+const UserProfile = (props) => {
+  console.log(props);
+  return (
+    <div style={{ border: "2px solid blue", padding: "5px", margin: "10px", backgroundColor: "rgba(220,220,220,1" }}>
+      <h2 style={{ color: "#5f3bab", fontWeight: "bold", fontSize: "30px", margin: "4px" }}>{props.name}</h2>
+      <p>Age: <span style={{ color: "#003bae", fontSize: "20px", margin: "4px" }}>{props.age}</span></p>
+      <p >Bio: <span style={{ color: "#bb3bfb", fontSize: "20px", margin: "4px" }}>{props.bio}</span></p>
+    </div>
+  );
+};
 
-  export default UserProfile;
+export default UserProfile;
